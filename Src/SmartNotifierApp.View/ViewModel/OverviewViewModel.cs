32991464f524e66eb76237379b1e7f1b06c06317
Shivam Srivastava <shivam01990@gmail.com>
2020-07-18 14:36:47 +0530

    Restart interval time

--- Src/SmartNotifierApp.View/ViewModel/OverviewViewModel.cs
+++ Src/SmartNotifierApp.View/ViewModel/OverviewViewModel.cs
@@ -4,12 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace SmartNotifier.View.ViewModel
 {
-  public class OverviewViewModel : ViewModelBase
+    public class OverviewViewModel : ViewModelBase
     {
-        public  string Name
+        public OverviewViewModel()
+        {
+
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            RestartInterval = NotifierDB.Instance.LastRestartedTimeSpan.ToString();
+        }
+
+        public string Name
         {
             get { return "Overview"; }
         }
@@ -19,12 +31,38 @@ namespace SmartNotifier.View.ViewModel
             get { return "/Files/Overview.png"; }
         }
 
+
+
+        private DispatcherTimer timer;
         public string LastRestartTime
         {
             get
             {
+                if (timer == null)
+                {
+                    timer = new DispatcherTimer();
+                    timer.Interval = TimeSpan.FromMilliseconds(1000);
+                    timer.Tick += timer_Tick;
+                    timer.Start();
+                }
                 return NotifierDB.Instance.LastRestartTime.ToString();
             }
         }
+
+        private string restartInterval;
+        public string RestartInterval
+        {
+            get
+            {
+                return restartInterval;
+            }
+            set
+            {
+                restartInterval = value;
+                RaisedPropertyChanged(nameof(RestartInterval));
+            }
+        }
+
+
     }
 }
